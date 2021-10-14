20051e140346fb56376b59f41f28ef40f8ad7fb8
twinaphex <libretro@gmail.com>
2021-10-14 19:26:42 +0200

    Fix Android build

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,7 @@ CORE_DIR := $(LOCAL_PATH)/..
 
 include $(CORE_DIR)/Makefile.common
 
-COREFLAGS := -std=gnu99 -DINLINE=inline -D__LIBRETRO__
+COREFLAGS := -std=gnu99 -DINLINE=inline -D__LIBRETRO__ $(INCFLAGS)
 
 GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
 ifneq ($(GIT_VERSION)," unknown")
